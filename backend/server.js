a5c1b5851e23e5193abac20b5cdd81a191d009a3
ashraftanin <ashraf.tanin@gmail.com>
2022-09-01 23:37:39 +0300

    Created user-model.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from './routers/userRouter.js';
+
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/lomani', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
 
 app.get('/', (req, res) => {
     res.send('Server is ready.');
 });
 
+app.use('/api/users', userRouter);
 app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
@@ -21,8 +29,13 @@ app.get('/api/products/:id', (req, res) => {
         res.status(404).send({message: 'Product not Found!'});
     }
     
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
 })
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('Serve at http://localhost:' + PORT);
 });
