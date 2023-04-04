27714beb9d1e9d8c1e0de8108dc055c71f22bf72
ashraftanin20 <67164399+ashraftanin20@users.noreply.github.com>
2023-04-04 23:42:51 +0300

    Update utils.js

--- backend/utils.js
+++ backend/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (user) => {
         isAdmin: user.isAdmin,
         isSeller: user.isSeller,
     }, 
-    process.env.JWT_SECRET || 'lomani@22', 
+    process.env.JWT_SECRET, 
     {
         expiresIn: '30d',
     }
@@ -22,7 +22,7 @@ export const isAuth = (req, res, next) => {
         const token = authorization.slice(7, authorization.length);
         jwt.verify(
             token,
-            process.env.JWT_SECRET || 'lomani@22',
+            process.env.JWT_SECRET,
             (err, decode) => {
                 if (err) {
                     res.status(401).send({message: 'Invalid Token'});
